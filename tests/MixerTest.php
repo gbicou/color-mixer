46cfa1ea3d42041139a74102fda8c43b9b323ccd
Benjamin VIELLARD <gbicou@gmail.com>
2025-01-21 17:45:07 +0100

    test yellow and blue make green

---
+++ tests/MixerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Bicou\ColorMixer\Couleur\CouleurMixer;
+use matthieumastadenis\couleur\ColorFactory;
+use PHPUnit\Framework\TestCase;
+
+class MixerTest extends TestCase
+{
+    public function testYellowAndBlueMakeGreen()
+    {
+        $yellow = ColorFactory::newRgb('#ffff00');
+        $blue = ColorFactory::newRgb('#0000ff');
+        $green = ColorFactory::newRgb('#388F54');
+
+        $middle = (new CouleurMixer($yellow, $blue))->atCouleur(0.5);
+
+        $this->assertEquals($green->toHexRgb()->coordinates(), $middle->toHexRgb()->coordinates());
+    }
+}
