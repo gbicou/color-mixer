69bb4331c8dab1bf5d769a98195445f48f31b903
Benjamin VIELLARD <gbicou@gmail.com>
2025-01-21 22:31:39 +0100

    couleur color simplification

--- src/Couleur/CouleurColor.php
+++ src/Couleur/CouleurColor.php
@@ -2,39 +2,20 @@
 
 namespace Bicou\ColorMixer\Couleur;
 
-use Bicou\ColorMixer\ColorBase;
+use Bicou\ColorMixer\Color;
 
 use matthieumastadenis\couleur\ColorInterface as CouleurColorInterface;
 
 /**
  * Color adapter
  */
-class CouleurColor extends ColorBase
+class CouleurColor extends Color
 {
-    private CouleurColorInterface $linear;
     public function __construct(
         CouleurColorInterface $color
     ) {
-        $this->linear = $color->toLinRgb();
-    }
+        $linear = $color->toLinRgb();
 
-    public function getRed(): float
-    {
-        return $this->linear->red;
-    }
-
-    public function getGreen(): float
-    {
-        return $this->linear->green;
-    }
-
-    public function getBlue(): float
-    {
-        return $this->linear->blue;
-    }
-
-    public function getOpacity(): float
-    {
-        return $this->linear->opacity;
+        parent::__construct($linear->red, $linear->green, $linear->blue, $linear->opacity);
     }
 }
