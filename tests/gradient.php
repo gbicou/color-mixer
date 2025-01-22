c75599cfc7bafbd1949273085a095a1828a3bad0
Benjamin VIELLARD <gbicou@gmail.com>
2025-01-22 17:36:32 +0100

    css gradient comparison

--- tests/gradient.php
+++ tests/gradient.php
@@ -5,8 +5,8 @@ require __DIR__.'/../vendor/autoload.php';
 use Bicou\ColorMixer\Couleur\CouleurMixer;
 use matthieumastadenis\couleur\colors\HexRgb;
 
-$from = new HexRgb('ff', 'cc', '00');
-$to = new HexRgb('00', '66', 'cc');
+$from = new HexRgb('ff', 'dd', '66');
+$to = new HexRgb('00', '66', 'ff');
 
 echo '<div>';
 
@@ -15,10 +15,14 @@ $mix = new CouleurMixer($from, $to);
 for ($i = 0; $i <= 1; $i += 0.2) {
     $m = $mix->atCouleur($i);
 
-    echo '<div style="background: ',$m->toHexRgb(),'; padding: 10px; margin: 2px">';
+    echo '<div style="background: ',$m->toHexRgb(),'; padding: 10px; margin: 1px">';
     echo $m->toHexRgb(),' ',$m->toLinRgb();
     echo '</div>';
 }
 
 echo '</div>';
 
+$from_css = $from->toHexRgb()->__toString();
+$to_css = $to->toHexRgb()->__toString();
+
+echo '<div style="margin-top: 10px; height: 120px; padding: 10px; background: linear-gradient(',$from_css,',', $to_css,')">CSS gradient</div>';
--- tests/srgb.php
+++ tests/srgb.php
@@ -3,7 +3,7 @@
 require __DIR__.'/../vendor/autoload.php';
 
 $from = new Bicou\ColorMixer\Color(1, 1, .1);
-$to = new Bicou\ColorMixer\Color(.1, .1, 1);
+$to = new Bicou\ColorMixer\Color(0, .3, 1);
 
 echo '<div>';
 
@@ -14,7 +14,7 @@ for ($i = 0; $i <= 1; $i += 0.2) {
 
     [$red, $green, $blue] = Bicou\ColorMixer\SRGBConversions::LinearToSRGB($m->getRed(), $m->getGreen(), $m->getBlue());
 
-    echo '<div style="background: color(srgb-linear ',$m->getRed(),' ',$m->getGreen(),' ',$m->getBlue(),'); padding: 10px; margin: 2px">';
+    echo '<div style="background: color(srgb-linear ',$m->getRed(),' ',$m->getGreen(),' ',$m->getBlue(),'); padding: 10px; margin: 1px">';
     // echo $red,' ',$green,' ',$blue;
     // echo "<br />";
     echo $m->getRed(),' ',$m->getGreen(), ' ',$m->getBlue();
@@ -22,3 +22,8 @@ for ($i = 0; $i <= 1; $i += 0.2) {
 }
 
 echo '</div>';
+
+$from_css = 'color(srgb-linear '.$from->getRed().' '.$from->getGreen().' '.$from->getBlue().')';
+$to_css = 'color(srgb-linear '.$to->getRed().' '.$to->getGreen().' '.$to->getBlue().')';
+
+echo '<div style="margin-top: 10px; height: 120px; padding: 10px; background: linear-gradient(',$from_css,',', $to_css,')">CSS gradient</div>';
