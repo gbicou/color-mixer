8c76e4443e7cef710bf3113ec39fbf7b108f219d
Benjamin VIELLARD <gbicou@gmail.com>
2025-01-31 15:53:53 +0100

    documentation

--- src/MixerImplementation.php
+++ src/MixerImplementation.php
@@ -3,12 +3,17 @@
 namespace Bicou\ColorMixer;
 
 /**
- * @template T
+ * A color mixer implementation with a color library.
+ *
+ * @template T the color interface from the library
  */
 interface MixerImplementation
 {
     /**
-     * @return T
+     * Mix colors based on the given ratio.
+     *
+     * @param float $ratio The ratio between the start (0.0) and stop (1.0) colors
+     * @return T The color object at the given ratio
      */
     public function mix(float $ratio): mixed;
 }
